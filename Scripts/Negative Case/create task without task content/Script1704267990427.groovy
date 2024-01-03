92983f099ecb282e9bf3e69408a3040eeabe9972
yahyahfidz <yahya@prieds.com>
2024-01-03 14:48:08 +0700

    create task without task content

---
+++ Scripts/Negative Case/create task without task content/Script1704267990427.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\LENOVO\\Downloads\\com.yoesuv.androidroom-v6(2.1.5)-production-release.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/negative case/android.widget.ImageButton'), 0)
+
+Mobile.tap(findTestObject('Object Repository/negative case/android.widget.EditText - Task Title'), 0)
+
+Mobile.setText(findTestObject('Object Repository/negative case/android.widget.EditText - Task Title (1)'), 'tugas hari ini', 
+    0)
+
+Mobile.tap(findTestObject('Object Repository/negative case/android.widget.Button - SAVE'), 0)
+
+'untuk verify icon warning muncul'
+Mobile.verifyElementExist(findTestObject('Object Repository/negative case/android.widget.ImageButton (1)'), 0)
+
+'untuk verify teks warning muncul'
+Mobile.verifyElementText(findTestObject('Object Repository/negative case/android.widget.TextView - Content is Empty'), 'Content is Empty')
+
+Mobile.closeApplication()
+
