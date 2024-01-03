b72a17aab66daf1b97bbc62b1b14158abce3614f
yahyahfidz <yahya@prieds.com>
2024-01-03 15:24:16 +0700

    delete all task

---
+++ Scripts/Positive Case/delete all task/Script1704269858564.groovy
@@ -0,0 +1,68 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\LENOVO\\Downloads\\com.yoesuv.androidroom-v6(2.1.5)-production-release.apk', true)
+
+Mobile.tap(findTestObject('delete all/android.widget.ImageButton'), 0)
+
+Mobile.tap(findTestObject('Object Repository/delete all/android.widget.EditText - Task Title'), 0)
+
+Mobile.setText(findTestObject('Object Repository/delete all/android.widget.EditText - Task Title (1)'), '1', 0)
+
+Mobile.tap(findTestObject('Object Repository/delete all/android.widget.EditText - Task Content'), 0)
+
+Mobile.setText(findTestObject('Object Repository/delete all/android.widget.EditText - Task Content (1)'), '2', 0)
+
+Mobile.tap(findTestObject('Object Repository/delete all/android.widget.Button - SAVE'), 0)
+
+Mobile.tap(findTestObject('android.widget.ImageButton'), 0)
+
+Mobile.tap(findTestObject('android.widget.EditText - Task Title (1)'), 0)
+
+Mobile.setText(findTestObject('android.widget.EditText - Task Title'), '3', 0)
+
+Mobile.tap(findTestObject('android.widget.EditText - Task Content (1)'), 0)
+
+Mobile.setText(findTestObject('android.widget.EditText - Task Content'), '4', 0)
+
+Mobile.tap(findTestObject('android.widget.Button - SAVE'), 0)
+
+Mobile.tap(findTestObject('android.widget.ImageButton'), 0)
+
+Mobile.tap(findTestObject('android.widget.EditText - Task Title (2)'), 0)
+
+Mobile.setText(findTestObject('android.widget.EditText - Task Title'), '5', 0)
+
+Mobile.tap(findTestObject('android.widget.EditText - Task Content (2)'), 0)
+
+Mobile.setText(findTestObject('android.widget.EditText - Task Content'), '6', 0)
+
+Mobile.tap(findTestObject('android.widget.Button - SAVE'), 0)
+
+Mobile.tap(findTestObject('Object Repository/delete all/android.widget.Button'), 0)
+
+'to verify delete all pop appeared'
+Mobile.verifyElementText(findTestObject('Object Repository/delete all/android.widget.TextView - Delete All Tasks'), 'Delete All Tasks?')
+
+Mobile.verifyElementVisible(findTestObject('Object Repository/delete all/android.widget.TextView - Delete All'), 0)
+
+Mobile.tap(findTestObject('Object Repository/delete all/android.widget.Button - OK'), 0)
+
+Mobile.closeApplication()
+
